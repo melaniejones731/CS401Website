30c2bc81a37534b7a45139fb178ef63be1eb419e
Melanie Jones <melaniejones@outlook.com>
2019-02-16 15:39:02 -0700

    experiment with capitalization

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ Site home page, composed of the templates below
 */
 
 //page header
-include("templates/header.html");
+include("Templates/header.html");
 //main content
 include("index.html"); 
 //carousel content
-include("templates/campCarousel.html");
+include("Templates/campCarousel.html");
 //footer
-include("templates/footer.html")
+include("Templates/footer.html")
 ?>

54c8032bc748af7a2eef07077092bafccee301e3
Melanie Jones <melaniejones@outlook.com>
2019-04-18 10:56:02 -0600

    carousel search works from any page

--- Templates/campCarousel.php
+++ Templates/campCarousel.php
@@ -2,19 +2,19 @@
         <div class="triCard">
         <img src="Styles/images/teens-robot464x309.jpg" style="width:100%">
                 <div class=container>
-                        <h2><a href="search-camps.php">Camps for Teens</h2></a>
+                        <h2><a href="teen-handler.php">Camps for Teens</h2></a>
                 </div>
         </div>
         <div class="triCard">
         <img src="Styles/images/sports-kids591x394.jpg" style="width:100%">
                 <div class=container>
-                        <h2><a href="search-camps.php">Athletic Camps</h2></a>
+                        <h2><a href="sport-handler.php">Athletic Camps</h2></a>
                 </div>
         </div>
         <div class="triCard">
         <img src="Styles/images/theater-kid488x325.jpg" style="width:100%">
                 <div class=container>
-                        <h2><a href="search-camps.php">Theater & Performance Camps</h2></a>
+                        <h2><a href="perform-handler.php">Theater & Performance Camps</h2></a>
                 </div>
         </div>
 </div>

4ea991a7fb917f1a877ddd47c576130a2485bb55
Melanie Jones <melaniejones@outlook.com>
2019-04-23 01:02:53 -0600

    debugging

--- controllers/Dao.php
+++ controllers/Dao.php
@@ -3,16 +3,16 @@
 
 class Dao {
     //local config
-    //private $host = "localhost";
-    //private $db = "TVKids";
-    //private $user = "melaniejones";
-    //private $pass = "password";
+    private $host = "localhost";
+    private $db = "TVKids";
+    private $user = "melaniejones";
+    private $pass = "password";
 
     //heroku config
-    private $host = "us-cdbr-iron-east-03.cleardb.net";
-    private $db = "heroku_9bd6986b3a0fe57";
-    private $user = "b934344d348a79";
-    private $pass = "0f8b4c56";
+    //private $host = "us-cdbr-iron-east-03.cleardb.net";
+    //private $db = "heroku_9bd6986b3a0fe57";
+    //private $user = "b934344d348a79";
+    //private $pass = "0f8b4c56";
     
     public function getConnection () {
         return new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,$this->pass);
@@ -21,13 +21,13 @@ class Dao {
     //camp search functions 
     public function getAllCamps () {
         $conn = $this->getConnection();
-        return $conn->query("SELECT DISTINCT Session.session_id, Session.start_date, Session.end_date, Session.cost, Session.isActive, Session.session_notes, Session.camp_location, Session.address_1, Session.address_2, Session.city, Session.state, Session.zipcode, Camp.camp_id, Camp.camp_name, Camp.camp_name, Camp.camp_location, Camp.address_1, Camp.address_2, Camp.city, Camp.state, Camp.zipcode, Camp.description, Camp.isActive, CampProvider.company_name, CampProvider.website, CampProvider.address_1, CampProvider.address_2, CampProvider.city, CampProvider.state, CampProvider.zipcode, CampProvider.isActive  FROM CampProvider Join Camp on CampProvider.provider_id=Camp.provider_id Join Session on Camp.camp_id = Session.camp_id ORDER BY Camp.camp_name");
+        return $conn->query("SELECT DISTINCT Session.session_id, Session.start_date, Session.end_date, Session.cost, Session.isActive, Session.session_notes, Session.camp_location, Session.address_1, Session.address_2, Session.city, Session.state, Session.zipcode, Camp.camp_id, Camp.camp_name, Camp.camp_name, Camp.camp_location, Camp.address_1, Camp.address_2, Camp.city, Camp.state, Camp.zipcode, Camp.description, Camp.isActive, CampProvider.company_name, CampProvider.website, CampProvider.address_1, CampProvider.address_2, CampProvider.city, CampProvider.state, CampProvider.zipcode, CampProvider.isActive  FROM CampProvider Join Camp on CampProvider.provider_id=Camp.provider_id Join Session on Camp.camp_id = Session.camp_id WHERE Session.isActive = 1 ORDER BY Camp.camp_name");
     }
 
     //camp search functions 
     public function getSearchedCamps ($searchString) {
         $conn = $this->getConnection();
-        $searchQuery ="SELECT DISTINCT Session.session_id, Session.start_date, Session.end_date, Session.cost, Session.isActive, Session.session_notes, Session.camp_location, Session.address_1, Session.address_2, Session.city, Session.state, Session.zipcode, Camp.camp_id, Camp.camp_name, Camp.camp_name, Camp.camp_location, Camp.address_1, Camp.address_2, Camp.city, Camp.state, Camp.zipcode, Camp.description, Camp.isActive, CampProvider.company_name, CampProvider.website, CampProvider.address_1, CampProvider.address_2, CampProvider.city, CampProvider.state, CampProvider.zipcode, CampProvider.isActive  FROM CampProvider Join Camp on CampProvider.provider_id=Camp.provider_id Join Session on Camp.camp_id = Session.camp_id Join CampCategory on Camp.camp_id = CampCategory.camp_id Join Category on CampCategory.category_id = Category.category_id Join CampAge on Camp.camp_id = CampAge.camp_id Join Age on CampAge.age_id = Age.age_id Join CampAttribute on Camp.camp_id = CampAttribute.camp_id Join Attribute on CampAttribute.attribute_id = attribute.attribute_id WHERE Session.isActive > 0 AND $searchString ORDER BY Camp.camp_name, Session.start_date";
+        $searchQuery ="SELECT DISTINCT Session.session_id, Session.start_date, Session.end_date, Session.cost, Session.isActive, Session.session_notes, Session.camp_location, Session.address_1, Session.address_2, Session.city, Session.state, Session.zipcode, Camp.camp_id, Camp.camp_name, Camp.camp_name, Camp.camp_location, Camp.address_1, Camp.address_2, Camp.city, Camp.state, Camp.zipcode, Camp.description, Camp.isActive, CampProvider.company_name, CampProvider.website, CampProvider.address_1, CampProvider.address_2, CampProvider.city, CampProvider.state, CampProvider.zipcode, CampProvider.isActive  FROM CampProvider Join Camp on CampProvider.provider_id=Camp.provider_id Join Session on Camp.camp_id = Session.camp_id Join CampCategory on Camp.camp_id = CampCategory.camp_id Join Category on CampCategory.category_id = Category.category_id Join CampAge on Camp.camp_id = CampAge.camp_id Join Age on CampAge.age_id = Age.age_id Join CampAttribute on Camp.camp_id = CampAttribute.camp_id Join Attribute on CampAttribute.attribute_id = attribute.attribute_id WHERE Session.isActive = 1 AND $searchString ORDER BY Camp.camp_name, Session.start_date";
         return $conn->query($searchQuery);
 
     }

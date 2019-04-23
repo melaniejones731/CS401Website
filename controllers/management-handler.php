97cae74018e77d1487651c40bb0adf3f2de1448f
Melanie Jones <melaniejones@outlook.com>
2019-04-23 00:57:20 -0600

    debugging

--- controllers/management-handler.php
+++ controllers/management-handler.php
@@ -11,7 +11,7 @@ if ((isset($_GET['id'])))
 {
     $sessionID =$_GET['id'];
     $sessionActive = $_GET['active'];
-    require_once 'controllers/Dao.php';
+    require_once 'Dao.php';
     $dao = new Dao();
     $active = $dao->getSessionStatus($sessionID);
 

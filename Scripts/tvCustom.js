334e9dda769a18f7f76cd84c19e73e83e3b0906b
Melanie Jones <melaniejones@outlook.com>
2019-04-23 00:34:22 -0600

    added jquery

--- Scripts/tvCustom.js
+++ Scripts/tvCustom.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    
+    console.log("do i exist?");
+
+    $("#datestart").change(function() {
+        var startDt= document.getElementById("datestart").value;
+        var endDt=document.getElementById("dateend").value;
+        console.log(startDt);
+        console.log(endDt);
+        if( (new Date(startDt).getTime() > new Date(endDt).getTime())){
+            alert("Oops! Your start date is after your end date - check your dates before clicking search.");
+        }
+      });
+
+      $("#dateend").change(function() {
+        var startDt= document.getElementById("datestart").value;
+        var endDt=document.getElementById("dateend").value;
+        console.log(startDt);
+        console.log(endDt);
+        if( (new Date(startDt).getTime() > new Date(endDt).getTime())){
+            alert("Oops! Your start date is after your end date - check your dates before clicking search.");
+        }
+      });
+  
 });
